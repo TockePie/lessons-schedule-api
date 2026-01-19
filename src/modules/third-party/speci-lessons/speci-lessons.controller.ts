56406059c6d5f46ff18e04311f08f22c08bba603
Maxim <144056146+TockePie@users.noreply.github.com>
2026-01-19 17:27:55 +0200

    speci lessons

--- src/modules/third-party/speci-lessons/speci-lessons.controller.ts
+++ src/modules/third-party/speci-lessons/speci-lessons.controller.ts
@@ -1,6 +1,12 @@
 import { UUID } from 'node:crypto'
 
-import { BadRequestException, Controller, Get, Query } from '@nestjs/common'
+import {
+  BadRequestException,
+  Controller,
+  Get,
+  NotFoundException,
+  Query
+} from '@nestjs/common'
 
 import { SpeciLessonsService } from './speci-lessons.service'
 
@@ -9,11 +15,20 @@ export class SpeciLessonsController {
   constructor(private readonly speciLessons: SpeciLessonsService) {}
 
   @Get()
-  getSpeciLessons(@Query('groupId') group_id: UUID) {
+  async getSpeciLessons(@Query('groupId') group_id: UUID) {
     if (!group_id) {
       throw new BadRequestException('Group ID is not provided.')
     }
 
-    return this.speciLessons.getSpeciLessons(group_id)
+    const response = await this.speciLessons.getSpeciLessons(group_id)
+
+    if (
+      response.scheduleFirstWeek.length === 0 &&
+      response.scheduleSecondWeek.length === 0
+    ) {
+      throw new NotFoundException('Group not found in external API')
+    }
+
+    return response
   }
 }
--- src/utils/filter-speci-lessons.ts
+++ src/utils/filter-speci-lessons.ts
@@ -1,13 +1,23 @@
+import { v5 as uuidv5 } from 'uuid'
+
 import {
   GroupScheduleDayItem,
   GroupScheduleResponse
 } from '../modules/third-party/speci-lessons/dto/schedule'
 
+const NAMESPACE = '6ba7b814-9dad-11d1-80b4-00c04fd430c8'
+
+//XXX: Used to generate uuid. Remove later
 const filterWeek = (week: GroupScheduleDayItem[]): GroupScheduleDayItem[] => {
   return week
     .map((day) => ({
       ...day,
-      pairs: day.pairs.filter((lesson) => lesson.dates.length > 0)
+      pairs: day.pairs
+        .filter((lesson) => lesson.dates.length > 0)
+        .map((lesson) => ({
+          ...lesson,
+          uuid: uuidv5(`${lesson.name} - ${lesson.teacherName}`, NAMESPACE)
+        }))
     }))
     .filter((day) => day.pairs.length > 0)
 }
