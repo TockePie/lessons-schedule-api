d7c6a63f167edee7f4457ccab4bbbcaaf02805ac
Maxim <144056146+TockePie@users.noreply.github.com>
2026-02-07 23:43:03 +0200

    todo

--- src/modules/schedule/schedule.service.ts
+++ src/modules/schedule/schedule.service.ts
@@ -1,8 +1,7 @@
 import { Injectable } from '@nestjs/common'
 
 import { PrismaService } from '../../config/prisma/prisma.service'
-import { weekParity } from '../../generated/prisma/client'
-import { scheduleWhereInput } from '../../generated/prisma/models'
+import { Prisma, weekParity } from '../../generated/prisma/client'
 
 @Injectable()
 export class ScheduleService {
@@ -34,10 +33,55 @@ export class ScheduleService {
     })
   }
 
-  getGroupSchedule(id: string, week?: 'even' | 'odd') {
+  getGroupSchedule(
+    id: string,
+    week?: 'even' | 'odd',
+    selectives: string[] = []
+  ) {
+    //TODO: Implement filtering by selectives
+
+    /** Case 1: filtering by group_id - should display all week parities and all selectives
+    SELECT * FROM public.schedule
+    INNER JOIN subject
+    ON schedule.subject_id = subject.subject_id
+    WHERE schedule.group_id = '480ada29-7f77-4ce9-96d7-01b47dfd5860'
+     */
+
+    /** Case 2: filtering by group_id and selectives - should display all week parities and selected selectives. We can pass week parity as an argument instead and it should display selected week parity and all selectives
+    SELECT * FROM public.schedule
+    INNER JOIN subject
+    ON schedule.subject_id = subject.subject_id
+    WHERE schedule.group_id = '480ada29-7f77-4ce9-96d7-01b47dfd5860'
+      AND (
+            schedule.subject_id IN (
+                'ca27e719-4553-41da-b2dc-91fda9e8509d',
+                '21b24bd8-54e6-4f7a-b1a7-bf8b3f79bff8'
+            )
+            OR subject.is_selective = false
+          );
+     */
+
+    /** Case 3: filtering by group_id, week_parity and selectives - should display selected week parity and selected selectives
+    SELECT * FROM public.schedule
+    INNER JOIN subject
+    ON schedule.subject_id = subject.subject_id
+    WHERE schedule.group_id = '480ada29-7f77-4ce9-96d7-01b47dfd5860'
+      AND (
+            schedule.subject_id IN (
+                'ca27e719-4553-41da-b2dc-91fda9e8509d',
+                '21b24bd8-54e6-4f7a-b1a7-bf8b3f79bff8'
+            )
+            OR subject.is_selective = false
+          )
+      AND (
+            schedule.week_parity = 'EVEN'
+            OR schedule.week_parity = 'BOTH'
+          );
+     */
+
     const weekParity = week?.toUpperCase() as weekParity
 
-    const where: scheduleWhereInput = {
+    const where: Prisma.scheduleWhereInput = {
       group_id: id
     }
 
